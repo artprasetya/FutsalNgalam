7db1f008e5005e466ca28d5175b59b32df17f3a9
PRAS <artiyanprasetya@gmail.com>
2017-07-09 17:07:28 +0700

    first

---
+++ app/src/main/java/com/example/asus/futsalngalam/Activity/ViewPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.asus.futsalngalam.Activity;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.asus.futsalngalam.Fragment.DetailFieldFragment;
+import com.example.asus.futsalngalam.Fragment.ReviewFragment;
+import com.example.asus.futsalngalam.R;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 2;
+    final Context context;
+
+    public ViewPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        switch (position){
+            case 0:
+                fragment = DetailFieldFragment.newInstance();
+                break;
+            case 1:
+                fragment = ReviewFragment.newInstance();
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return context.getResources().getString(R.string.tab_1);
+            case 1:
+                return context.getResources().getString(R.string.tab_2);
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+}
--- app/src/main/java/com/example/asus/futsalngalam/Fragment/ReviewFragment.java
+++ app/src/main/java/com/example/asus/futsalngalam/Fragment/ReviewFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.asus.futsalngalam.R;
 
@@ -14,18 +13,19 @@ import com.example.asus.futsalngalam.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class ReviewFragment extends Fragment {
 
-    private Button detail1, detail2, detail3;
 
-    public HomeFragment() {
-        // Required empty public constructor
+    public static ReviewFragment newInstance(){
+        return new ReviewFragment();
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_about, container, false);
     }
+
 }
